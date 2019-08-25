24db5527fc33f7c4655a956b68cf814afac88d4d
aanchalpatial <apatial.27@gmail.com>
2019-08-25 14:11:44 +0530

    played video on the tracked plane using sprite kit and scene kit

--- AugmentedReality/NewspaperViewController.swift
+++ AugmentedReality/NewspaperViewController.swift
@@ -48,11 +48,22 @@ class NewspaperViewController: UIViewController, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
         if let imageAnchor = anchor as? ARImageAnchor{
+            
+            let videoNode = SKVideoNode(fileNamed: "potter.mp4")
+            videoNode.play()
+            let videoScene = SKScene(size: CGSize(width: 1280, height: 720))
+            videoNode.position = CGPoint(x: videoScene.size.width/2, y: videoScene.size.height/2)
+            videoNode.yScale = -1.0
+            videoScene.addChild(videoNode)
+            
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.3)
+            plane.firstMaterial?.diffuse.contents = videoScene
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -Float.pi/2
             node.addChildNode(planeNode)
+            
+            
+            
         }
         return node
     }
